182066a7d0a064b36afb0fafebd72411c2ffdc04
llan <llan@amwares.com>
2017-12-07 14:54:20 +0800

    添加图片鼠标跟随效果

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React, { Component } from 'react';
-import 'animate.css';
 import './App.css';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import Main from './Main/Main';
 class App extends Component {
     render() {
         return (
             <div className="App">
-              <ReactCSSTransitionGroup
-                  transitionName="background"
-                  transitionEnter={true}
-                  transitionLeave={false}
-                  transitionEnterTimeout={500}>
-                  <div key="background" className="fadeIn animated background">
-                    <main className="animated bounceInDown">
-                      <blockquote>
-                        我明白了，拥有强大力量人的最大的弱点就是力量本身。
-                        <footer>-
-                          <cite>
-                            武藤游戏 &laquo;游戏王&raquo;
-                          </cite>
-                        </footer>
-                      </blockquote>
-                    </main>
-                    <img key="cartoon-left" className="cartoon-left" src="//oymcd876b.bkt.clouddn.com/cartoon_left.jpg" alt="cartoon_left"/>
-                    <img key="cartoon-right" className="cartoon-right" src="//oymcd876b.bkt.clouddn.com/cartoon_right.jpg" alt="cartoon_right"/>
-                  </div>
-              </ReactCSSTransitionGroup>
+              <Main/>
             </div>
         );
     }
---
+++ src/Main/Main.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import './Main.css';
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isHover: false,
+            isLeft: false,
+            isTop: false
+        }
+        this.handleHoverOff = this.handleHoverOff.bind(this);
+        this.handleHoverOn = this.handleHoverOn.bind(this);
+    };
+    handleHoverOn() {
+        this.setState({ isHover: true });
+    }
+    handleMouseMove(ref, e) {
+        const element = this.refs[ref];
+        const { left, right, top, bottom } = element.getBoundingClientRect();
+        const { pageX, pageY } = e;
+        const isLeft = pageX > left && pageX < (right - left) / 2 + left;
+        const isTop = pageY > top && pageY < (bottom - top) / 2 + top;
+        isLeft ? this.setState({ isLeft: true }) : this.setState({ isLeft: false });
+        isTop ? this.setState({ isTop: true }) : this.setState({ isTop: false });
+    }
+    handleHoverOff() {
+        this.setState({ isHover: false });
+    }
+    render() {
+        const isLeftClassName = this.state.isLeft ? "cartoon-go-right" : "cartoon-go-left";
+        const isTopClassName = this.state.isTop ? "cartoon-go-down" : "cartoon-go-top";
+        return (
+            <ReactCSSTransitionGroup
+                transitionName="background"
+                transitionEnter={true}
+                transitionLeave={false}
+                transitionEnterTimeout={500}>
+                <div className="fadeIn animated background">
+                  <main>
+                    <blockquote>
+                      我明白了，拥有强大力量人的最大的弱点就是力量本身。
+                      <footer>-
+                        <cite>
+                          武藤游戏 &laquo;游戏王&raquo;
+                        </cite>
+                      </footer>
+                    </blockquote>
+                  </main>
+                  <img
+                    onMouseMove = {(e)=>this.handleMouseMove('cartoon-left',e)}
+                    onMouseEnter={this.handleHoverOn}
+                    onMouseLeave={this.handleHoverOff}
+                    ref="cartoon-left"
+                    className={
+                      this.state.isHover?`cartoon-left ${isLeftClassName} ${isTopClassName}`:"cartoon-left"
+                    }
+                    src="//oymcd876b.bkt.clouddn.com/cartoon_left.jpg"
+                    alt="cartoon_left"
+                  />
+                  <img
+                    onMouseMove = {(e)=>this.handleMouseMove('cartoon-left',e)}
+                    onMouseEnter={this.handleHoverOn}
+                    onMouseLeave={this.handleHoverOff}
+                    className={
+                      this.state.isHover?`cartoon-right ${isLeftClassName} ${isTopClassName}`:"cartoon-right"
+                    }
+                    src="//oymcd876b.bkt.clouddn.com/cartoon_right.jpg"
+                    alt="cartoon_right"
+                  />
+                </div>
+            </ReactCSSTransitionGroup>
+        )
+    }
+}
+export default Main;
